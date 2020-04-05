f6b455a0ff38fb4a6d288dcf6f29bb07f553c173
KingJ <kj@kingj.net>
2020-04-05 14:33:51 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ PACKAGE_NAME = 'oem-database-anidb-tvdb'
 def build_config():
     config = {
         'name': PACKAGE_NAME,
-        'version': '1.19.22',
+        'version': '1.19.23',
 
         'author': 'Dean Gardiner',
         'author_email': 'me@dgardiner.net',
